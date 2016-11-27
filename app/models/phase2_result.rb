aa3a318c381358faceded2b8fe5456159f3c9ee9
Li Ling <lilinglynn0315@gmail.com>
2016-11-27 14:55:01 +0000

    sorting algo & basic styling & spreadsheet loop

--- app/models/phase2_result.rb
+++ app/models/phase2_result.rb
@@ -1,2 +1,13 @@
 class Phase2Result < ActiveRecord::Base
+    belongs_to :Phase2s
+    
+        def self.to_csv(options = {})
+        attributes = %w{name nric school class_name email phone final_selection}
+        CSV.generate(options) do |csv|
+            csv << {name: 'Name', nric: 'NRIC', school: 'School', class_name: 'Class', email: 'Email', phone: 'Phone', final_selection: 'Final_selection'}.values
+                all.each do |phase2_result|
+            csv << {name: 'Name', nric: 'NRIC', school: 'School', class_name: 'Class', email: 'Email', phone: 'Phone', final_selection: 'Final_selection'}.keys.map{ |attr| phase2_result.send(attr) }
+        end
+      end
+    end
 end
